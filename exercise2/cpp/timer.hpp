655941fe0bd4792c944ce729eb8178b3a0149c0b
GUAIGUAIYI <953309026@qq.com>
2025-09-29 23:52:31 +0800

    guaiguaiyi exercise1 and exercise2

--- exercise2/cpp/timer.hpp
+++ exercise2/cpp/timer.hpp
@@ -11,23 +11,42 @@ template <typename T = std::chrono::milliseconds> struct Timer {
 
   Clock::time_point start_time_wall;
 
-  Timer() {}
-  ~Timer() {
-    // Print
+  Timer() { 
+    start_time_wall = Clock::now();  // 构造时开始计时
   }
-  void reset() { /* reset start time */ }
+  
+  ~Timer() {
+    std::cout << *this;  // 析构时自动输出
+  }
+  
+  void reset() { 
+    start_time_wall = Clock::now();  // 重置开始时间
+  }
 
   T elapsed() const {
-    // Return during time
+    auto now = Clock::now();
+    return std::chrono::duration_cast<T>(now - start_time_wall);
   }
 
   friend std::ostream &operator<<(std::ostream &os, const Timer &timer) {
-    // TODO:
+    auto duration = timer.elapsed();
+    os << "Wall: " << duration.count() << time_unit_name<T>();
+    return os;
   }
 
   template <typename U> static constexpr std::string_view time_unit_name() {
-    if constexpr (std::is_same_v<U, std::chrono::seconds>)
-      return "s"; // Support More units
+    if constexpr (std::is_same_v<U, std::chrono::hours>)
+      return "h";
+    else if constexpr (std::is_same_v<U, std::chrono::minutes>)
+      return "min";
+    else if constexpr (std::is_same_v<U, std::chrono::seconds>)
+      return "s";
+    else if constexpr (std::is_same_v<U, std::chrono::milliseconds>)
+      return "ms";
+    else if constexpr (std::is_same_v<U, std::chrono::microseconds>)
+      return "us";
+    else if constexpr (std::is_same_v<U, std::chrono::nanoseconds>)
+      return "ns";
     return "?";
   }
 }; // Timer
